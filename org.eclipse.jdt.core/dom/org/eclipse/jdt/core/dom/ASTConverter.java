b614b3c830ffb2b40643d56deee1ea96c20bccce
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-01-20 13:33:12 +0100

    Prevent crash in AST-to-DOM converter

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
@@ -2059,6 +2059,9 @@ class ASTConverter {
 		if (expression instanceof org.eclipse.jdt.internal.compiler.ast.SwitchExpression) {
 			return convert((org.eclipse.jdt.internal.compiler.ast.SwitchExpression) expression);
 		}
+		if (expression instanceof org.eclipse.jdt.internal.compiler.ast.OldExpression) {
+			return convert(((org.eclipse.jdt.internal.compiler.ast.OldExpression)expression).expression);
+		}
 		return null;
 	}
 
