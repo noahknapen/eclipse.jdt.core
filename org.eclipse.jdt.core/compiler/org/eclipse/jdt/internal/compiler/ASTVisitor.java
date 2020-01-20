d03571134550ff2eee7fe022a54b0c31d0957ef4
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-01-20 16:04:57 +0100

    Generate code for 'old' expressions

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ASTVisitor.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ASTVisitor.java
@@ -520,6 +520,9 @@ public abstract class ASTVisitor {
 	public void endVisit(IntersectionCastTypeReference intersectionCastTypeReference, BlockScope scope) {
 		// do nothing by default
 	}
+	public void endVisit(OldExpression oldExpression, BlockScope scope) {
+		// do nothing by default
+	}
 	public void endVisit(SwitchExpression switchExpression,	BlockScope scope) {
 		// do nothing by default
 	}
@@ -1027,6 +1030,9 @@ public abstract class ASTVisitor {
 		return true; // do nothing by default, keep traversing
 	}
 	public boolean visit(IntersectionCastTypeReference intersectionCastTypeReference, BlockScope scope) {
+		return true; // do nothing by default, keep traversing
+	}
+	public boolean visit(OldExpression oldExpression, BlockScope blockScope) {
 		return true; // do nothing by default, keep traversing
 	}
 	public boolean visit(SwitchExpression switchExpression, BlockScope blockScope) {
