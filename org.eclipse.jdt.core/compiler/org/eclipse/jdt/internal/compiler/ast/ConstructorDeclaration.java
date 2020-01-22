4993de4d7a1f0490d91272d3ba871f73fe549978
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-01-22 10:21:12 +0100

    Keep checking code out of method body
    Fixes semantic highlighting regression

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ConstructorDeclaration.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ConstructorDeclaration.java
@@ -172,6 +172,9 @@ public void analyseCode(ClassScope classScope, InitializationFlowContext initial
 		flowInfo.setReachMode(nonStaticFieldInfoReachMode);
 
 		// propagate to statements
+		if (this.formalSpecification != null)
+			flowInfo = this.formalSpecification.analyseCode(this.scope, constructorContext, flowInfo);
+		
 		if (this.statements != null) {
 			CompilerOptions compilerOptions = this.scope.compilerOptions();
 			boolean enableSyntacticNullAnalysisForFields = compilerOptions.enableSyntacticNullAnalysisForFields;
@@ -453,6 +456,9 @@ private void internalGenerateCode(ClassScope classScope, ClassFile classFile) {
 			}
 		}
 		// generate statements
+		if (this.formalSpecification != null)
+			this.formalSpecification.generateCode(this.scope, codeStream);
+
 		if (this.statements != null) {
 			for (int i = 0, max = this.statements.length; i < max; i++) {
 				this.statements[i].generateCode(this.scope, codeStream);
