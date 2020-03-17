d7151110c832296f9023d1b2cb39c4f980fd8f8f
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-03-17 13:51:23 +0100

    Fix https://github.com/fsc4j/fsc4j/issues/10

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/AbstractMethodDeclaration.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/AbstractMethodDeclaration.java
@@ -580,7 +580,7 @@ public abstract class AbstractMethodDeclaration
 			if (this.formalSpecification != null)
 				this.formalSpecification.resolve();
 			resolveStatements();
-			if (this.formalSpecification != null && this.formalSpecification.postconditions != null) {
+			if (this.formalSpecification != null && this.formalSpecification.postconditions != null && !this.ignoreFurtherInvestigation) {
 				this.formalSpecification.postconditionMethodCall.resolve(this.scope);
 				this.formalSpecification.postconditionVariableDeclaration.binding.useFlag = LocalVariableBinding.USED;
 			}
