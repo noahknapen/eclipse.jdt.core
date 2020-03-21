1d045ab1248e61c87bd1f5614b140729186fb87f
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-03-21 21:01:15 +0100

    Parse @invar tag formal parts; typecheck same in method docs
    TODO: Typecheck @invar tag formal parts in class, field docs

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/Parser.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/Parser.java
@@ -5254,6 +5254,7 @@ private Annotation[] consumeAnnotations(AbstractMethodDeclaration md) {
 			if (md != null)
 				md.formalSpecification = new FormalSpecification(md);
 			ArrayList<Annotation> annotations = new ArrayList<>();
+			ArrayList<Expression> invariants = new ArrayList<>();
 			ArrayList<Expression> preconditions = new ArrayList<>();
 			ArrayList<Expression> throwsConditions = new ArrayList<>();
 			ArrayList<Expression> mayThrowConditions = new ArrayList<>();
@@ -5265,6 +5266,7 @@ private Annotation[] consumeAnnotations(AbstractMethodDeclaration md) {
 				else {
 					FormalSpecificationClause clause = (FormalSpecificationClause)e;
 					switch (clause.tag) {
+						case INVAR: invariants.add(clause.expression); break;
 						case PRE: preconditions.add(clause.expression); break;
 						case THROWS: throwsConditions.add(clause.expression); break;
 						case MAY_THROW: mayThrowConditions.add(clause.expression); break;
@@ -5282,6 +5284,8 @@ private Annotation[] consumeAnnotations(AbstractMethodDeclaration md) {
 				result = null;
 			// TODO(fsc4j): Report a problem if md == null?
 			if (md != null) {
+				if (!invariants.isEmpty())
+					md.formalSpecification.invariants = invariants.toArray(new Expression[invariants.size()]);
 				if (!preconditions.isEmpty())
 					md.formalSpecification.preconditions = preconditions.toArray(new Expression[preconditions.size()]);
 				if (!throwsConditions.isEmpty())
