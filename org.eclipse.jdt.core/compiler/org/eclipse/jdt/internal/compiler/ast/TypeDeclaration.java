a08f87b4f5a2b45484af52e5215618e28ef3b75f
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-03-22 09:01:09 +0100

    Typecheck @invar formal parts in field docs

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
@@ -1390,6 +1390,7 @@ public void resolve() {
 			}
 		}
 		if (this.fields != null) {
+			boolean hasInvariants = false;
 			for (int i = 0, count = this.fields.length; i < count; i++) {
 				FieldDeclaration field = this.fields[i];
 				switch(field.getKind()) {
@@ -1424,7 +1425,14 @@ public void resolve() {
 						break;
 				}
 				field.resolve(field.isStatic() ? this.staticInitializerScope : this.initializerScope);
+				if (field.invariants != null)
+					hasInvariants = true;
 			}
+			if (hasInvariants)
+				for (FieldDeclaration field : this.fields)
+					if (field.invariants != null)
+						for (Expression e : field.invariants)
+							e.resolveTypeExpecting(this.initializerScope, TypeBinding.BOOLEAN);
 		}
 		if (this.maxFieldCount < localMaxFieldCount) {
 			this.maxFieldCount = localMaxFieldCount;
