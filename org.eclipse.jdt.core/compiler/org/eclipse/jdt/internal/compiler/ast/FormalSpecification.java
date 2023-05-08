noahknapen/eclipse.jdt.core

commit 7e1ec93be70db3c81e1ddd3b4609dc7f0f43cda5

Implemented more detailed checking of @may_throws clauses: checks now happen based on the exception type specified in the @may_throws clause, analog to @throws clauses. TODO: Clauses without an exception type are simply ignored, report this as a compiler time error that an exception type is necessary.

org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/FormalSpecification.java
@@ -381,9 +381,12 @@ public class FormalSpecification {
 					this.method.statements = new Statement[] {outerBlock};
 					this.method.explicitDeclarations = 0;
 				}
-
+				
 				if (this.mayThrowConditions != null) {
 					for (int i = 0 ; i < this.mayThrowConditions.length ; i++) {
+						if (this.mayThrowExceptionTypeNames[i] == null) {
+							continue;
+						}
 						Expression e = this.mayThrowConditions[i];
 						//e.resolveTypeExpecting(this.method.scope, TypeBinding.BOOLEAN);
 						
@@ -398,10 +401,25 @@ public class FormalSpecification {
 					noMayThrowConditionsSatisfiedBlock.statements = new Statement[] {loggerMessage,
 							new ThrowStatement(new SingleNameReference(LAMBDA_PARAMETER2_NAME, (this.method.bodyStart << 32) + this.method.bodyStart), this.method.sourceStart, this.method.sourceEnd)
 					};
-					Statement statement = noMayThrowConditionsSatisfiedBlock;
+					Statement statement = new EmptyStatement(0,0);
 					for (int i = 0; i < this.mayThrowConditions.length; i++) {
+						if (this.mayThrowExceptionTypeNames[i] == null) {
+							continue;
+						}
+						// Put an if-statement with expression instanceof exception. When true, check if the condition for that exception is satisfied.
+						// When no instanceof statement is true, simply continue execution (it is MAY throw, not MUST throw so is legal to not specify an exception)
+						// When instanceof is true and condition is satisfied, do NOT throw caught exception as the @throws clauses have to be checked too
+						// When instanceof is true and condition is NOT satisfied, generate a logger message and throw exception
 						Expression e = this.mayThrowConditions[i];
-						statement = new IfStatement(e, new EmptyStatement(0,0), statement, e.sourceStart, e.sourceEnd);
+						Expression condition = new InstanceOfExpression(
+								new SingleNameReference(LAMBDA_PARAMETER2_NAME, (this.method.bodyStart << 32) + this.method.bodyStart),
+								this.mayThrowExceptionTypeNames[i]
+						);
+						Block thenBlock = new Block(0);
+						thenBlock.statements = new Statement[] {
+							new IfStatement(e, new EmptyStatement(0,0), noMayThrowConditionsSatisfiedBlock, e.sourceStart, e.sourceEnd)
+						};
+						statement = new IfStatement(condition, thenBlock, statement, e.sourceStart, e.sourceEnd);
 					}	
 					postconditionBlockStatements.add(statement);
 				}	

commit f55da13a24e43466f27a373998422488ae77af7e

Fixed bug so multiple @may_throw clauses are again supported. This feature was broken by commit 7e1ec93be70db3c81e1ddd3b4609dc7f0f43cda5

org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/FormalSpecification.java
@@ -397,10 +397,7 @@ public class FormalSpecification {
 					blockDeclarationsCount += 2 * oldExpressions.size();
 					
 					MessageSend loggerMessage = generateSevereLoggerMessage(maythrowconditionAssertionMessage);
-					Block noMayThrowConditionsSatisfiedBlock = new Block(0);
-					noMayThrowConditionsSatisfiedBlock.statements = new Statement[] {loggerMessage,
-							new ThrowStatement(new SingleNameReference(LAMBDA_PARAMETER2_NAME, (this.method.bodyStart << 32) + this.method.bodyStart), this.method.sourceStart, this.method.sourceEnd)
-					};
+					
 					Statement statement = new EmptyStatement(0,0);
 					for (int i = 0; i < this.mayThrowConditions.length; i++) {
 						if (this.mayThrowExceptionTypeNames[i] == null) {
@@ -415,6 +412,10 @@ public class FormalSpecification {
 								new SingleNameReference(LAMBDA_PARAMETER2_NAME, (this.method.bodyStart << 32) + this.method.bodyStart),
 								this.mayThrowExceptionTypeNames[i]
 						);
+						Block noMayThrowConditionsSatisfiedBlock = new Block(0);
+						noMayThrowConditionsSatisfiedBlock.statements = new Statement[] {loggerMessage,
+								new ThrowStatement(new SingleNameReference(LAMBDA_PARAMETER2_NAME, (e.sourceStart << 32) + e.sourceStart), e.sourceStart, e.sourceEnd)
+						};
 						Block thenBlock = new Block(0);
 						thenBlock.statements = new Statement[] {
 							new IfStatement(e, new EmptyStatement(0,0), noMayThrowConditionsSatisfiedBlock, e.sourceStart, e.sourceEnd)